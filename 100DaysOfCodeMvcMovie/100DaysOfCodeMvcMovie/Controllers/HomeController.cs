5b83aa639acee7f83e4fcd299f63591be073c3dd
Nurhak Kaya <Nurhak.Kaya@felinesoft.com>
2021-09-07 21:56:48 +0100

    unneccessary usings removed

--- 100DaysOfCodeMvcMovie/100DaysOfCodeMvcMovie/Controllers/HomeController.cs
+++ 100DaysOfCodeMvcMovie/100DaysOfCodeMvcMovie/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using _100DaysOfCodeMvcMovie.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace _100DaysOfCodeMvcMovie.Controllers
 {
