a87b2c404da04cd72398643f32b704c2e8f83b72
Nurhak Kaya <Nurhak.Kaya@felinesoft.com>
2021-09-09 20:03:14 +0100

    search code changes done

--- 100DaysOfCodeMvcMovie/100DaysOfCodeMvcMovie/Controllers/MoviesController.cs
+++ 100DaysOfCodeMvcMovie/100DaysOfCodeMvcMovie/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using _100DaysOfCodeMvcMovie.Data;
 using _100DaysOfCodeMvcMovie.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace _100DaysOfCodeMvcMovie.Controllers
 {
@@ -17,17 +18,39 @@ namespace _100DaysOfCodeMvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string id)
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
             var movies = from m in _context.Movie
                          select m;
 
-            if (!string.IsNullOrEmpty(id))
+            if (!string.IsNullOrEmpty(searchString))
             {
-                movies = movies.Where(x => x.Title.Contains(id));
+                movies = movies.Where(s => s.Title.Contains(searchString));
             }
 
-            return View(await movies.ToListAsync());
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
+        }
+
+        [HttpPost]
+        public string Index(string searchString, bool notUsed)
+        {
+            return "From [HttpPost]Index: filter on " + searchString;
         }
 
         // GET: Movies/Details/5
