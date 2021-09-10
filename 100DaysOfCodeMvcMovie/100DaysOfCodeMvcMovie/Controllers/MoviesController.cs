b449e0729da58924068f3c53733053dee12fe063
Nurhak Kaya <Nurhak.Kaya@felinesoft.com>
2021-09-10 10:32:04 +0100

    final commit after adding some dev notes

--- 100DaysOfCodeMvcMovie/100DaysOfCodeMvcMovie/Controllers/MoviesController.cs
+++ 100DaysOfCodeMvcMovie/100DaysOfCodeMvcMovie/Controllers/MoviesController.cs
@@ -167,6 +167,23 @@ namespace _100DaysOfCodeMvcMovie.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
+            #region Dev notes
+
+            // The common language runtime(CLR) requires overloaded methods to have a unique parameter signature(same method name but different list of parameters).
+            // However, here you need two Delete methods --one for GET and one for POST-- that both have the same parameter signature. (They both need to accept a single integer as a parameter.)
+
+            // There are two approaches to this problem,
+            // one is to give the methods different names.That's what the scaffolding mechanism did in the preceding example.
+            // However, this introduces a small problem:
+            // ASP.NET maps segments of a URL to action methods by name, and if you rename a method, routing normally wouldn't be able to find that method.
+            // The solution is what you see in the example, which is to add the ActionName("Delete") attribute to the DeleteConfirmed method.
+            // That attribute performs mapping for the routing system so that a URL that includes / Delete / for a POST request will find the DeleteConfirmed method.
+
+            // Another common work around for methods that have identical names and signatures is to artificially change the signature of the POST method to include an extra(unused) parameter.
+            // That's what we did in a previous post when we added the notUsed parameter. You could do the same thing here for the [HttpPost] Delete method:
+
+            #endregion Dev notes
+
             var movie = await _context.Movie.FindAsync(id);
             _context.Movie.Remove(movie);
             await _context.SaveChangesAsync();
